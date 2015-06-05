869ae7aa4bf6f85a6febc15709b738815f11dbd4
SenseiKiwi <SenseiKiwi@users.noreply.github.com>
2015-06-04 21:56:39 -0500

    Added keyboard controls
    Added code to MainForm for controlling the player's character with the
    arrow keys.

--- NinjaGame/src/MainForm.Designer.cs
+++ NinjaGame/src/MainForm.Designer.cs
@@ -28,21 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.physicsTimer = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
+            // 
+            // physicsTimer
+            // 
+            this.physicsTimer.Interval = 50;
+            this.physicsTimer.Tick += new System.EventHandler(this.physicsTimer_Tick);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(484, 462);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MinimizeBox = false;
             this.Name = "MainForm";
             this.Text = "Ninja Game";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.MainForm_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MainForm_KeyDown);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Timer physicsTimer;
     }
 }
 
