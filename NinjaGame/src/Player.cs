c2063cda27c8677a63df46f0a32cb7ad5b199280
Jaimetted <Jaimettedm4@hotmail.com>
2015-06-04 22:27:16 -0400

    Class Player
    Created class Player

---
+++ NinjaGame/src/Player.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace NinjaGame.src
+{
+    class Player
+    {
+        public Point Position { get; set; }
+
+
+        public Player()
+        {
+            Position = new Point(0,0);
+        }
+
+    }
+}
