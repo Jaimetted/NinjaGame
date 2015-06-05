290963fbcc4ae6859b0eefe99952c16b1e63b74d
Jaimetted <Jaimettedm4@hotmail.com>
2015-06-05 01:06:43 -0400

    Added Projectile feature
    Changed Player (int -> float)
    Added Class Vector, Projectile

--- NinjaGame/src/Player.cs
+++ NinjaGame/src/Player.cs
@@ -3,21 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace NinjaGame
 {
     class Player
     {
-        public int X { get; set; }
-        public int Y { get; set; }
-
+        public float X { get; set; }
+        public float Y { get; set; }
+        public Vector Direction { get; set; }
         public Player()
         {
             X = 0;
             Y = 0;
+            Direction = new Vector(0,1);
         }
 
-        public void Move(int moveX,int moveY)
+        public void Move(float moveX,float moveY)
         {
             X += moveX;
             Y += moveY;
---
+++ NinjaGame/src/Vector.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NinjaGame
+{
+    class Vector
+    {
+        public float X { get; private set; }
+        public float Y { get; private set; }
+
+        public Vector()
+        {
+            X = 0;
+            Y = 0;
+        }
+        public Vector(float startX, float startY)
+        {
+            X = startX;
+            Y = startY;
+        }
+
+        public Vector Multiply(float scale)
+        {
+            return new Vector(X * scale, Y * scale);
+        }
+    }
+}
