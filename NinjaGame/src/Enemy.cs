5e7d8a5830027ce5dc73579553f5a908efc11661
Jaimetted <Jaimettedm4@hotmail.com>
2015-06-06 03:02:08 -0400

    Random enemy movement
    Added Random enemy movement

--- NinjaGame/src/Enemy.cs
+++ NinjaGame/src/Enemy.cs
@@ -10,11 +10,20 @@ namespace NinjaGame
     {
         public float X { get; set; }
         public float Y { get; set; }
+        public int direction { get; set; }
+
+        public Vector Direction { get; set; }
 
         public Enemy(float startX, float startY)
         {
             X = startX;
             Y = startY;
         }
+        public void Move(float moveX, float moveY)
+        {
+            X += moveX;
+            Y += moveY;
+        }
+        
     }
 }
